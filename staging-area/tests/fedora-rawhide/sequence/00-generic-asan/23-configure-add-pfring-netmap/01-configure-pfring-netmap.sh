2c6e1af500ded92cfadf8f9585c95271dae156fd
Peter Manev <pmanev@stamus-networks.com>
2017-05-11 16:57:51 +0300

    fedora: fix clang ref in the combined pfring/netmap check

--- staging-area/tests/fedora-rawhide/sequence/00-generic-asan/23-configure-add-pfring-netmap/01-configure-pfring-netmap.sh
+++ staging-area/tests/fedora-rawhide/sequence/00-generic-asan/23-configure-add-pfring-netmap/01-configure-pfring-netmap.sh
@@ -28,7 +28,7 @@ if [ ! -f ${SEQUENCE_DIR}/SKIP_PFRING_NETMAP ]; then
     ./autogen.sh
     
     ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var \
-    CC=clang-3.9 CFLAGS="-ggdb3 -Werror -Wchar-subscripts -fno-strict-aliasing -fstack-protector-all -fsanitize=address -fno-omit-frame-pointer -Wno-unused-parameter -Wno-unused-function" \
+    CC=clang CFLAGS="-ggdb3 -Werror -Wchar-subscripts -fno-strict-aliasing -fstack-protector-all -fsanitize=address -fno-omit-frame-pointer -Wno-unused-parameter -Wno-unused-function" \
     ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes \
     --enable-unittests --enable-debug --enable-profiling \
     --enable-nfqueue --enable-nflog --enable-pfring --enable-netmap \
